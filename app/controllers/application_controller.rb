12cda5a559dd0209cc92080d974aca739052c4da
darthj <darthj93@gmail.com>
2018-12-10 16:59:30 -0600

    Add role to users and assign admin and guest to sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :categories, :brands
 
-  	def categories
-  		@categories = Category.order(:name)
-  	end
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to main_app.product_url, :alert => "Not authorized!" }
+    end
+  end
+  
+  def categories
+  	@categories = Category.order(:name)
+  end
 
 	def brands
 	  @brands = Product.pluck(:brand).sort.uniq
 	end
-protected
+  
+  protected
+  
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up, keys: [])
-  	devise_parameter_sanitizer.permit(:account_update, keys: [])
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,12 +14,14 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
+    authorize! :create, @product
     @product = Product.new
     @categories = Category.all 
   end
 
   # GET /products/1/edit
   def edit
+    authorize! :update, @product
     @categories = Category.all
   end
 
@@ -56,6 +58,7 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    authorize! :destroy, @product
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
